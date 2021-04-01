e8b63e51a96188d846d5acb87402592f8f07c2f1
GabrielQuispe <gabrielomarquisperosas@gmail.com>
2021-04-01 16:53:41 -0500

    list of cards

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { ListOfCategories } from "./components/ListOfCategories";
 import {GlobalStyle} from './GlobalStyles';
+import { ListOfPhotoCards } from "./components/ListOfPhotoCards";
 
 const App = () => {
   return (
     <div className="">
       <GlobalStyle />
       <ListOfCategories />
+      <ListOfPhotoCards />
     </div>
   );
 }
