970f6df2c8ba64b15573191d6ee0747782b190cf
GabrielQuispe <gabrielomarquisperosas@gmail.com>
2021-04-20 21:15:32 -0500

    apollo client context

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Img, ImgWrapper, Article } from './styles';
-import { useLocalStorage } from '../../hooks/useLocalStorage';
 import { useNearScreen } from '../../hooks/useNearScreen';
 import { FavButton } from '../FavButton';
 import { ToggleLikeMutation } from '../../container/ToggleLikeMutation';
@@ -8,9 +7,7 @@ import { Link } from '@reach/router';
 
 const DEFAULT_IMAGE = "https://images.unsplash.com/photo-1500879747858-bb1845b61beb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60";
 
-export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
-    const key = `like${id}`;
-    const [liked, setLiked] = useLocalStorage(key, false);
+export const PhotoCard = ({ id,liked, likes = 0, src = DEFAULT_IMAGE }) => {
     const { show, ref } = useNearScreen();
 
 
@@ -26,14 +23,13 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
                     <ToggleLikeMutation>
                         {(toggleLike) => {
                             const handleFavClick = () => {
-                                !liked && toggleLike({
+                                 toggleLike({
                                     variables: {
                                         input: {
                                             id
                                         }
                                     }
                                 })
-                                setLiked(!liked);
                             }
                             return <FavButton liked={liked} likes={likes} onClick={handleFavClick} />
 
--- src/index.js
+++ src/index.js
@@ -3,21 +3,36 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ApolloProvider } from 'react-apollo';
-import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-boost'
 import { ContextProvider} from './context/Context';
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
 
-const cache = new InMemoryCache()
 
-const link = new HttpLink({
-    uri: 'https://petgram-server-jrmfsd-okxluew9o.now.sh/graphql',
-    headers: {
-      authorization: `Bearer ${sessionStorage.getItem('token')}` || '',
-    }
+const httpLink = createHttpLink({
+  uri: 'https://petgram-server-jrmfsd-okxluew9o.now.sh/graphql',
+
 })
 
+const authLink = setContext((_, { headers }) => {
+  const token = window.sessionStorage.getItem('token')
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    },
+  }
+} )
+
 const client = new ApolloClient({
-  cache,
-  link,
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+  onerror: error => {
+    const { networkError } = error
+    if (networkError && networkError.result.code === 'invalid_token') {
+      window.sessionStorage.removeItem('token')
+      window.location.href = '/'
+    }
+  }
 })
 
 ReactDOM.render(
